8b8acbb5c9bc7d29813bcf40d86b760f076873a5
lendominik <len.dominik13@gmail.com>
2025-04-02 20:28:49 +0200

    create UserService and IUserService

---
+++ UserManager/UserManager.API/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using UserManager.API.Dtos.User;
+
+namespace UserManager.API.Interfaces;
+
+public interface IUserService
+{
+    Task<IEnumerable<GetUserDto>> GetAll(int page, int pageSize);
+    Task<GetUserDto?> Get(int id);
+    public Task<int> Add(CreateUserDto user);
+    public Task<bool> Update(int id, UpdateUserDto user);
+    public Task<bool> Delete(int id);
+}
---
+++ UserManager/UserManager.API/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using UserManager.API.Entities;
+using UserManager.API.Interfaces;
+
+namespace UserManager.API.Services;
+
+public class UserService : IUserService
+{
+    public IEnumerable<User> GetAll()
+    {
+        return [];
+    }
+
+    public User? Get()
+    {
+        return null;
+    }
+
+    public void Add()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Delete()
+    {
+
+    }
+}
