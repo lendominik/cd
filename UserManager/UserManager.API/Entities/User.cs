88651e3cf9bacafd753cbf0dcfbb6a82fd0e30f6
lendominik <len.dominik13@gmail.com>
2025-04-02 20:29:56 +0200

    add User entity

---
+++ UserManager/UserManager.API/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManager.API.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public string? PhoneNumber { get; set; }
+}
